1b3028fbc61d9ee05e312957aaab52bea56a463b
Sorenon <sorenonandstuffs@gmail.com>
2022-11-19 22:05:10 +0000

    k

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import * as core from '@actions/core'
 import { context, getOctokit } from '@actions/github';
 import {wait} from './wait'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('millisecondsagain')
-    core.debug("why is this not showing up")
-    core.debug(`${process.env}`)    
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
-}
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('millisecondsagain')
+//     core.debug("why is this not showing up")
+//     core.debug(`${process.env}`)    
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
 
 run()
