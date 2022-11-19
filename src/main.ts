096723200bd41c1f790d25f4fd0d7c7872231374
Sorenon <sorenonandstuffs@gmail.com>
2022-11-19 21:59:54 +0000

    testing

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    core.debug("why is this not showing up")
     core.debug(`${process.env}`)    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
