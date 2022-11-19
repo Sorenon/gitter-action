0d6b3390dbcfb9e60e1c8927f713852a02769547
Sorenon <sorenonandstuffs@gmail.com>
2022-11-19 23:21:04 +0000

    fair enough

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,12 @@ async function run(): Promise<void> {
     // throw new Error('I am annoyed')
     core.info("why is this not showing up")
     core.info(`${JSON.stringify(process.env)}`)
+    core.info(`${JSON.stringify(context)}`)
     core.notice("I'm a notice")
     core.error("I'm an error")
     core.warning("I'm a warning")
+
+    
 
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
